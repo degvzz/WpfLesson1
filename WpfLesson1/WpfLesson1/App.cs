229812f6829ffa2cd2492ba4bcc1976e2fb22291
DESKTOP-522V4I2\Eco <degvzzdbdb@naver.com>
2023-05-08 18:43:09 +0900

    Code Upload.

---
+++ WpfLesson1/WpfLesson1/App.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using WpfLibrary1;
+
+namespace WpfLesson1
+{
+    internal class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            BasicWindow win = new();
+            win.ShowDialog();
+        }
+    }
+}
---
+++ WpfLesson1/WpfLibrary1/BasicViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfLibrary1
+{
+    public class BasicViewModel
+    {
+        public string Name { get; set; }
+        public bool IsChecked { get; set; }
+
+        public BasicViewModel()
+        {
+            Name = "James";
+            IsChecked = true;
+        }
+    }
+}
